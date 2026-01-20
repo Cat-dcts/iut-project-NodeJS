4e8d2faf57abc84246035a5e3b69c32c4929f391
Cathy Descoutures <cathy.descoutures@etu.unilim.fr>
2026-01-20 17:51:45 +0100

    Configure MySQL database connection

--- server/manifest.js
+++ server/manifest.js
@@ -3,11 +3,11 @@
 const Dotenv = require('dotenv');
 const Confidence = require('@hapipal/confidence');
 const Toys = require('@hapipal/toys');
+const Schwifty = require('@hapipal/schwifty');
 
-// Pull .env into process.env
+// Charge .env dans process.env
 Dotenv.config({ path: `${__dirname}/.env` });
 
-// Glue manifest as a confidence store
 module.exports = new Confidence.Store({
     server: {
         host: 'localhost',
@@ -32,6 +32,32 @@ module.exports = new Confidence.Store({
             {
                 plugin: '../lib', // Main plugin
                 options: {}
+            },
+            {
+                plugin: '@hapipal/schwifty',
+                options: {
+                    $filter: 'NODE_ENV',
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: {
+                            client: 'mysql2',
+                            connection: {
+                                host: process.env.DB_HOST || '127.0.0.1',
+                                user: process.env.DB_USER || 'root',
+                                password: process.env.DB_PASSWORD || 'hapi',
+                                database: process.env.DB_DATABASE || 'user',
+                                port: Number(process.env.DB_PORT) || 3306
+                            },
+                            migrations: {
+                                stub: Schwifty.migrationsStubPath
+                            }
+                        }
+                    },
+                    production: {
+                        migrateOnStart: false
+                    }
+                }
             },
             {
                 plugin: {
